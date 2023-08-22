c8afc3d18592cf7c811b2c32ec9958d46677d04c
Akhtar Alzrouni <138444303+AkhtarAlzarouni@users.noreply.github.com>
2023-08-22 22:09:16 +0400

    hbkh

--- bin/dart_application_4.dart
+++ bin/dart_application_4.dart
@@ -3,9 +3,9 @@ import 'dart:io';
 import 'package:test/test.dart';
 
 void main() {
-  Map <String,dynamic> mac={'id':1,'name':'macBook','size':500,'price':200,'processor':'i5','camera':'3d'};
-  Map <String,dynamic> hp={'id':2,'name':'HP','size':700,'price':250,'processor':'i7','camera':'2d'};
-  Map <String,dynamic> dell={'id':3,'name':'Dell','size':300,'price':300,'processor':'i7','camera':'3d'};
+  Map <String,dynamic> mac={'id':1,'name':'macBook','size':500,'price':200.0,'processor':'i5','camera':'3d'};
+  Map <String,dynamic> hp={'id':2,'name':'HP','size':700,'price':250.0,'processor':'i7','camera':'2d'};
+  Map <String,dynamic> dell={'id':3,'name':'Dell','size':300,'price':300.0,'processor':'i7','camera':'3d'};
 
   List<Map> prodcts=[mac,hp,dell];
   for (var element in prodcts) {
@@ -13,11 +13,14 @@ void main() {
     
   }
 
-print("Choose a product number:");
+print("Write your budget:");
    double? p=double.tryParse(stdin.readLineSync() ?? "0");
    var nowlast=prodcts.where((element) => element['price']<=p).toList();
-   print(nowlast);
-  
+   
+   for (var e in nowlast) {
+    print(e['name'] +" : "+ e['price'].toString());
+     
+   }
 
 }
   
